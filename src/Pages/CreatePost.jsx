b9fdac100b68727c1e64b7a04c14ab4e052c173b
batman <mohaiman.abdul@intellier.com>
2023-01-23 17:45:43 +0600

    home compo added

--- src/Pages/CreatePost.jsx
+++ src/Pages/CreatePost.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
 
 const CreatePost = () => {
   return (
